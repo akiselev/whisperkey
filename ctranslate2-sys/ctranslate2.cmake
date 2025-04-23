c829f6433e16549b0483ebdddbd570c297935d10
Alexander Kiselev <github@akiselev.com>
2025-04-23 00:17:39 -0700

    updates

--- ctranslate2-sys/ctranslate2.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.19)
-# Enable find_package debug tracing
-set(CMAKE_FIND_DEBUG_MODE ON)
-
-# Call find_package (adjust the package name and REQUIRED flag as needed)
-find_package(CTranslate2 REQUIRED)
-
-# Optionally, print a message if found
-message("CTranslate2 found: \${CTranslate2_FOUND}")
